3aa89041efb5061ef737cfb5fb73120efd5ea8ed
Pierpaolo Laurito <pierpaolo.laurito@gmail.com>
2025-03-16 23:08:39 +0100

    Fix: Risolto problema con service worker nel build del frontend

--- salus-frontend/src/service-worker.js
+++ salus-frontend/src/service-worker.js
@@ -1,1 +1,23 @@
-// Non posso modificare questo file direttamente perché non è nella lista dei file disponibili. 
+/* eslint-disable no-restricted-globals */
+
+// This service worker can be customized
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/js/main.bundle.js',
+        '/static/css/main.bundle.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+}); 
