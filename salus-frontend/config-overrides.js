6b446910ed28787793500d0f388bca142243d854
Pierpaolo Laurito <pierpaolo.laurito@gmail.com>
2025-03-19 18:13:35 +0100

    Style: Migliorata completamente l'interfaccia grafica con design moderno e professionale

--- salus-frontend/config-overrides.js
+++ salus-frontend/config-overrides.js
@@ -1,21 +1,36 @@
-const { override, addWebpackModuleRule } = require('customize-cra');
+const { override, addWebpackAlias, addPostcssPlugins, adjustStyleLoaders } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
-  addWebpackModuleRule({
-    test: /\.css$/,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
+  // Aggiungiamo alias per facilitare le importazioni
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@styles': path.resolve(__dirname, 'src/styles'),
+    '@assets': path.resolve(__dirname, 'src/assets')
+  }),
+  
+  // Aggiungiamo plugin PostCSS per migliorare il supporto cross-browser
+  addPostcssPlugins([
+    require('autoprefixer'),
+    require('postcss-flexbugs-fixes')
+  ]),
+  
+  // Configura correttamente i loader di stile
+  adjustStyleLoaders(({ use }) => {
+    if (use && use.length > 0) {
+      const cssLoader = use.find(loader => loader.loader && loader.loader.includes('css-loader'));
+      if (cssLoader) {
+        cssLoader.options = {
+          ...cssLoader.options,
+          url: true,
+          import: true,
           modules: {
             auto: true,
             localIdentName: '[name]__[local]--[hash:base64:5]'
           }
-        }
-      },
-      'postcss-loader'
-    ]
+        };
+      }
+    }
   })
 ); 
